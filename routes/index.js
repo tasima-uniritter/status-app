24fb606e10456edad532c9b17145b50b96bdc12c
Hyan Mandian <hyanmandian@hotmail.com>
2018-07-07 11:11:10 -0300

    Adjust stringify output

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var version = { version: '1.0.0', status: 'OK', name: 'topi-status-real' }
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(version));
+  res.send(JSON.stringify(version, null, 2));
 });
 
 module.exports = router;
