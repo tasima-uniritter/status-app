c3071cc6db6fff18b44a44fc7d9bfecaf07b84f7
Hyan Mandian <hyanmandian@hotmail.com>
2018-07-07 11:08:23 -0300

    Update file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var version = { version: '1.0.0', status: 'OK' }
+  var version = { version: '1.0.0', status: 'OK', name: 'topi-status-real' }
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(version));
 });
